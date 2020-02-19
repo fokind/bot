bfacf9e36fe77dd6e2636d4233f08044c1621374
Dmitry Fokin <fokind1@yandex.ru>
2020-02-19 21:55:06 +0300

    через статическое свойство

--- index.js
+++ index.js
@@ -10,16 +10,21 @@ require("express-ws")(app);
 
 const port = process.env.PORT;
 app.use(express.static("./webapp"));
-const botServer = new BotServer();
 app.use("/odata", BotServer.create());
+
+const botServer = new BotServer();
+const eventEmitter = BotServer.eventEmitter;
 
 app.ws("/ws", ws => {
   // по подписке на событие новых данных выдавать новые данные на фронт
-  botServer.on("time", e => {
-    ws.send(e);
+  eventEmitter.on("time", e => {
+    ws.send(e); // проверить, что клиент доступен
   });
 
-  botServer.start();
+  botServer.start(); // выполнять по какой-то команде с фронта
+  // команда может приходить непосредствено на сервер или через ws
+  // т.к. и odata и ws это один и тот же сервер с разными интерфейсами доступа, то не должно иметь значения что за команда
+  // ws просто обрабатывает события и передает их клиенту, не более и наоборот
 });
 
 app.listen(port, () => {
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,19 @@
 import es from "event-stream";
+import { EventEmitter } from "events";
 import moment from "moment";
 import { Edm, odata, ODataServer } from "odata-v4-server";
 
 @odata.cors
 @odata.namespace("Bot")
 export class BotServer extends ODataServer {
-  public start() {
+  public static eventEmitter: EventEmitter = new EventEmitter();
+
+  @Edm.ActionImport()
+  public async start(): Promise<void> {
+    const eventEmitter = BotServer.eventEmitter;
+
     setInterval(() => {
-      this.emit("time", moment().toISOString());
+      eventEmitter.emit("time", moment().toISOString());
     }, 1000);
   }
 }
