da3d40c267fe411b5ae12a093885b247a4c4ad2b
Dmitry Fokin <fokind1@yandex.ru>
2020-02-28 23:12:56 +0300

    работает UI

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,46 +1,55 @@
-sap.ui.define(["sap/ui/core/UIComponent", "sap/ui/core/ws/WebSocket", "sap/ui/Device"], function(
-  UIComponent,
-  WebSocket, Device
-) {
-  "use strict";
+sap.ui.define(
+  ["sap/ui/core/UIComponent", "sap/ui/core/ws/WebSocket", "sap/ui/Device"],
+  function(UIComponent, WebSocket, Device) {
+    "use strict";
 
-  return UIComponent.extend("fokind.bot.Component", {
-    metadata: {
-      manifest: "json"
-    },
+    return UIComponent.extend("fokind.bot.Component", {
+      metadata: {
+        manifest: "json"
+      },
 
-    init: function() {
-      UIComponent.prototype.init.apply(this, arguments);
-    //   var oRouter = this.getRouter();
-    //           oRouter.initialize();
-      var oModel = this.getModel();
-      oModel.setData({
-        messages: []
-      });
+      init: function() {
+        UIComponent.prototype.init.apply(this, arguments);
+        var oModel = this.getModel();
+        oModel.setData({
+          Candles: []
+        });
 
-      var ws = new WebSocket("/ws");
-      ws.attachMessage(function(oEvent) {
-        var aMessages = oModel.getProperty("/messages");
-        var oMessage = JSON.parse(oEvent.getParameter("data"));
-        switch (oMessage.name) {
+        var ws = new WebSocket("/ws");
+        ws.attachMessage(function(oEvent) {
+          var oMessage = JSON.parse(oEvent.getParameter("data"));
+          switch (oMessage.name) {
             case "ticker":
-                oModel.setProperty("/Ticker", oMessage.data);
-                break;
+              oModel.setProperty("/Ticker", oMessage.data);
+              break;
             case "candles":
-                var aCandles = oModel.getProperty("Candles");
-                aCandles.push(oMessage.data);
-                oModel.refresh();
+              var aCandles = oModel.getProperty("/Candles");
+              var iLength = aCandles.length;
+              var aData = oMessage.data;
+              for (var i = 0; i < aData.length; i++) {
+                var oData = aData[i];
+                var oCandle = aCandles.filter(function(e) {
+                  return e.time === oData.time;
+                })[0];
+                if (oCandle) {
+                  var iIndex = aCandles.indexOf(oCandle);
+                  oModel.setProperty("/Candles/" + iIndex, oData);
+                } else {
+                  oModel.setProperty("/Candles/" + iLength++, oData);
+                }
+              }
+          }
+        });
+      },
+
+      getContentDensityClass: function() {
+        if (!this._sContentDensityClass) {
+          this._sContentDensityClass = Device.support.touch
+            ? "sapUiSizeCozy"
+            : "sapUiSizeCompact";
         }
-  });
-    },
-    
-    getContentDensityClass: function() {
-      if (!this._sContentDensityClass) {
-        this._sContentDensityClass = Device.support.touch
-          ? "sapUiSizeCozy"
-          : "sapUiSizeCompact";
+        return this._sContentDensityClass;
       }
-      return this._sContentDensityClass;
-    }
-  });
-});
+    });
+  }
+);
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,13 +1,11 @@
-sap.ui.define([
-	'sap/ui/core/mvc/Controller'
-], function (Controller) {
-	"use strict";
+sap.ui.define(["sap/ui/core/mvc/Controller"], function(Controller) {
+  "use strict";
 
-	return Controller.extend("fokind.bot.controller.App", {
-	     onInit: function() {
-        this.getView().addStyleClass(
-          this.getOwnerComponent().getContentDensityClass()
-        );
-      }
-	});
+  return Controller.extend("fokind.bot.controller.App", {
+    onInit: function() {
+      this.getView().addStyleClass(
+        this.getOwnerComponent().getContentDensityClass()
+      );
+    }
+  });
 });
